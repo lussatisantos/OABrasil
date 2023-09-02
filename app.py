3da67290d2797dd023d6a80a6b97924e86aa61e6
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-02 15:20:47 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+# entrar no site https://pje-consulta-publica.tjmg.jus.br/pje/ConsultaPublica/listView.seam
+# digitar numero oab e selecionar estado
+# clicar em pesquisar
+# entrar em cada um do processo
+#extrair o n* processo e data de distribuicao
+# extrair e guardas todos os ultimos moviementos
+# guardar tudo no excel, separados por processo
+

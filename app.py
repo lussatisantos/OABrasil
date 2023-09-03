79b24ea13e0ca41e56b6222949f1b75684ea78d4
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-03 14:49:15 +0100

    config

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ numero_oab = 133864
 # entrar no site https://pje-consulta-publica.tjmg.jus.br/pje/ConsultaPublica/listView.seam
 driver = webdriver.Chrome()
 driver.get('https://pje-consulta-publica.tjmg.jus.br/pje/ConsultaPublica/listView.seam')
-print()
 sleep(30)
 
 # digitar numero oab
@@ -22,6 +21,10 @@ opcoes_estados = Select(dropdown_estados)
 opcoes_estados.select_by_visible_text('SP')
 
 # clicar em pesquisar
+botao_pesquisar = driver.find_element(By.XPATH, "//input[@id='fPP:searchProcessos']")
+botao_pesquisar.click()
+sleep(10)
+
 # entrar em cada um do processo
 #extrair o n* processo e data de distribuicao
 # extrair e guardas todos os ultimos moviementos

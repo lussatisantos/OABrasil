0e59e45b4e3b0bede1dd6d754f9dcfd3435d071d
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-03 14:18:31 +0100

    config

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
 # entrar no site https://pje-consulta-publica.tjmg.jus.br/pje/ConsultaPublica/listView.seam
 # digitar numero oab e selecionar estado
 # clicar em pesquisar

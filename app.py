4ea543ba54a975031e591f11510750a930f4a12b
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-03 14:43:34 +0100

    pegar dados apartir do selenium

--- app.py
+++ app.py
@@ -4,12 +4,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from time import sleep
 
+numero_oab = 133864
+
 # entrar no site https://pje-consulta-publica.tjmg.jus.br/pje/ConsultaPublica/listView.seam
 driver = webdriver.Chrome()
 driver.get('https://pje-consulta-publica.tjmg.jus.br/pje/ConsultaPublica/listView.seam')
 print()
 sleep(30)
-# digitar numero oab e selecionar estado
+
+# digitar numero oab
+campo_oab = driver.find_element(By.XPATH, "input[@id='fPP:Decoration:numeroOAB']")
+campo_oab.send_keys(numero_oab)
+
+#Selecionar estado
+dropdown_estados = driver.find_element(By.XPAT, "//select[@id='fPP:Decoration:estadoComboOAB']")
+opcoes_estados = Select(dropdown_estados)
+opcoes_estados.select_by_visible_text('SP')
+
 # clicar em pesquisar
 # entrar em cada um do processo
 #extrair o n* processo e data de distribuicao

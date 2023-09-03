cf87fcbbf5ba3506c8cff9f96310a3923636ad1b
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-03 14:33:17 +0100

    open chrome

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from time import sleep
 
 # entrar no site https://pje-consulta-publica.tjmg.jus.br/pje/ConsultaPublica/listView.seam
+driver = webdriver.Chrome()
+driver.get('https://pje-consulta-publica.tjmg.jus.br/pje/ConsultaPublica/listView.seam')
+print()
+sleep(30)
 # digitar numero oab e selecionar estado
 # clicar em pesquisar
 # entrar em cada um do processo
